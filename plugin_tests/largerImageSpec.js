05444bc7082c7483b79a6dfcc7c217e84bd4aaeb
Christian Suloway <christian.suloway@nih.gov>
2018-10-09 14:07:14 -0400

    stub in Jasmine client tests

---
+++ plugin_tests/largerImageSpec.js
@@ -0,0 +1,40 @@
+girderTest.importPlugin('jobs');
+girderTest.importPlugin('worker');
+girderTest.importPlugin('large_image');
+girderTest.importPlugin('larger_image');
+girderTest.startApp();
+
+$(function () {
+    describe('Test the histogram plugin', function () {
+        it('create the admin user', function () {
+            girderTest.createUser(
+                'admin', 'admin@email.com', 'Admin', 'Admin', 'testpassword')();
+        });
+        it('go to collections page', function () {
+            runs(function () {
+                $("a.g-nav-link[g-target='collections']").click();
+            });
+
+            waitsFor(function () {
+                return $('.g-collection-create-button:visible').length > 0;
+            }, 'navigate to collections page');
+
+            runs(function () {
+                expect($('.g-collection-list-entry').length).toBe(0);
+            });
+        });
+        it('create collection', girderTest.createCollection('test', '', 'image'));
+        it('upload test file', function () {
+            girderTest.waitForLoad();
+            runs(function () {
+                $('.g-folder-list-link:first').click();
+            });
+            girderTest.waitForLoad();
+            runs(function () {
+                girderTest.binaryUpload('plugins/large_image/plugin_tests/test_files/grey10kx5k.tif');
+            });
+            girderTest.waitForLoad();
+            // TODO
+        });
+    });
+});
