d2c30ea15dc39dc0dcfdbbfa2e968e35807ae27e
Miao <tymiao@gwmail.gwu.edu>
2021-02-18 14:35:05 -0500

    add openslide supported

--- girder_larger_image/tilesource/__init__.py
+++ girder_larger_image/tilesource/__init__.py
@@ -1,7 +1,9 @@
 from large_image.tilesource import AvailableTileSources
 from large_image.exceptions import TileSourceException
 from .tiff import TiffFileTileSource, TiffGirderTileSource
+from large_image_source_openslide import girder_source
 
+AvailableTileSources['openslide'] = girder_source.OpenslideGirderTileSource
 AvailableTileSources['tifffile'] = TiffFileTileSource
 AvailableTileSources['tiff'] = TiffGirderTileSource
 
