227dca40bf911f2da9625f884045d4d2c6615dd7
tymiao1220 <tymiao@gwmail.gwu.edu>
2021-06-01 10:30:08 -0400

    replace get region route

--- girder_larger_image/rest/tilesEx.py
+++
@@ -1,6 +0,0 @@
-from girder_large_image.rest.tiles import ImageMimeTypes, \
-    TilesItemResource
-
-class TilesEx(TilesItemResource):
-    def getTilesRegion(self, item, params):
-        regionData = super(AnnotationResource, self).getTilesRegion(item, params)
